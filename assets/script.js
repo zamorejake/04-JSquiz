959be5c06d2ae7aa0c92bbdb106c9c5e0001283c
Jake <zamorejake@gmail.com>
2023-04-13 21:17:31 -0500

    Added boxes with content that randomize

--- assets/script.js
+++ assets/script.js
@@ -15,9 +15,75 @@ function countDown() {
         }
     }, 1000);
 }
-score.addEventListener("click", highScore);
+/*submitButton.addEventListener("click", question);
 function highScore() {
 
 
-}
+}*/
 
+
+    const sample = {
+        question: "Q1",
+        answer: "1",
+        b: "2",
+        c: "3",
+        d: "4"
+    };
+
+    const sample2 = {
+        question: "Q2",
+        answer: "5",
+        b: "6",
+        c: "7",
+        d: "8"
+    };
+
+    const sample3 = {
+        question: "Q3",
+        answer: "9",
+        b: "10",
+        c: "11",
+        d: "12"
+    };
+
+    const sample4 = {
+        question: "Q4",
+        answer: "13",
+        b: "14",
+        c: "15",
+        d: "16"
+    };
+
+    var setQ = sample.question;
+    var setAns = sample.answer;
+    var setB = sample.b;
+    var setC = sample.c;
+    var setD = sample.d;
+
+    let mainQuestion = document.getElementById("mainQuestion");
+    mainQuestion.append(setQ);
+
+    let mainAns = document.getElementById("mainAns");
+    mainAns.append(setAns);
+    mainAns.style.order = Math.floor(Math.random() * 4);
+    let mainB = document.getElementById("mainB");
+    mainB.append(setB);
+    mainB.style.order = Math.floor(Math.random() * 4);
+    let mainC = document.getElementById("mainC");
+    mainC.append(setC);
+    mainC.style.order = Math.floor(Math.random() * 4);
+    let mainD = document.getElementById("mainD");
+    mainD.append(setD);
+    mainD.style.order = Math.floor(Math.random() * 4);
+    
+/*function question() {
+    let example = [sample, sample2, sample3, sample4];
+    let qBank = example.splice(Math.floor(Math.random() * example.length),1);
+    console.log(qBank);
+    console.log(example);
+} */
+
+function scoring() {
+    let score = 0;
+
+}
